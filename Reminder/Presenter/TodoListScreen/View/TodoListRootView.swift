819ef6abd3475638e631b1f7bc326d54bfbb885f
Rob-Yoo <jin12243@naver.com>
2024-07-02 18:01:05 +0900

    Chore: .gitginore 생성

---
+++ Reminder/Presenter/TodoListScreen/View/TodoListRootView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoListRootView.swift
+//  Reminder
+//
+//  Created by Jinyoung Yoo on 7/2/24.
+//
+
+import UIKit
+
+class TodoListRootView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
